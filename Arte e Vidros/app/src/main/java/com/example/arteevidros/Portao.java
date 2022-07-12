6287c560c54f83a3dea14c5ce77e44e3a4dac054
AnnaFiameti <anna_fiameti@estudante.sc.senai.br>
2022-07-12 09:39:34 -0300

    Versao1

---
+++ Arte e Vidros/app/src/main/java/com/example/arteevidros/Portao.java
@@ -0,0 +1,14 @@
+package com.example.arteevidros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Portao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portao);
+    }
+}
